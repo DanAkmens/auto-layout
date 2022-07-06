9445a66f2147a7971c580d1d5fd0f90e26471a59
d.putans <dainis.putans@baptisthealth.net>
2022-07-06 19:45:36 +0300

    design with size classes

---
+++ size-classes-04/Size-classes/Size-classes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Size-classes
+//
+//  Created by d.putans on 04/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
